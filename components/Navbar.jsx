be5cfca12875179ba757787825c60fd18cfa5123
Mathieu JOLY <m.joly@live.fr>
2021-03-17 12:03:58 +0100

    add Navbar Layout

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export const Navbar = () => {
+  return (
+    <>
+      <nav className='Navbar'>
+	      <a>test1</a>
+	      <a>test2</a>
+      </nav>
+    </>
+  )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <Layout>
+    <div>
+      <Head>
+        <title>extra-resto - Home</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div>Hello World</div>
+    </div>
+    </Layout>
+  );
+}
