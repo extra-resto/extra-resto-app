3c563bcd391813bd691e28556a04e692dae37be3
Mathieu JOLY <m.joly@live.fr>
2021-03-20 14:18:34 +0100

    update login

--- components/FormLogin/index.jsx
+++ components/FormLogin/index.jsx
@@ -1,35 +1,96 @@
 import Router from 'next/router';
 import styles from './FormLogin.module.scss';
+import { useState, useEffect } from 'react';
+import { setUser, setEmployer } from 'store/User/userAction';
+import { useDispatch } from 'react-redux';
 
 const FormLogin = () => {
-  const handleLogin = async event => {
-    event.preventDefault()
+  const dispatch = useDispatch();
+  const [form, setForm] = useState({ 
+    email: '',
+    password: ''
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errors, setErrors] = useState({email: ''});
 
-    const res = await fetch('https://extra-resto-api.herokuapp.com/api/login', {
-      body: JSON.stringify({
-        user: {
-          email: event.target.email.value,
-          password: event.target.password.value
-        }
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
+  useEffect(() => {
+    if (isSubmitting) {
+      if (errors.email === '') {
+        handleLogin();
+      }
+      else {
+        setIsSubmitting(false);
+      }
+    }
+  }, [errors])
+
+  const handleLogin = async () => {
+    try {
+      const req = await fetch('http://localhost:3000/api/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ form })
+      })
+      const token = req.headers.get('Authorization');
+      dispatch(setUser(token))
+      const result = await req.json();
+      Router.push("/");
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let errs = validate();
+    setErrors(errs);
+    setIsSubmitting(true);
+  }
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
     })
+  }
 
-    const result = await res.json();
-    Router.push('/');
+  const validate = () => {
+
+    let err = { email: '' };
+
+    if(form.email.match(/^[A-z]+@[A-z]+.[A-z]+$/) === null) {
+      err.email = 'Veuillez entrer une adresse email valide'
+    }
+    
+    return err;
   }
 
   return (
-    <form className={styles.FormLogin} onSubmit={handleLogin}>
+    <>
+    {errors.email ? <p>Veuillez entrer une adresse email valide</p> : null}
+    <form className={styles.FormLogin} onSubmit={handleSubmit}>
       <label htmlFor="email">Email</label>
-      <input id="email" name="email" type="email" autoComplete="email" required />
+      <input 
+        name="email"
+        type="email"
+        autoComplete="email"
+        onChange={handleChange}
+        required
+      />
       <label htmlFor="password">Password</label>
-      <input id="password" name="password" type="password" autoComplete="password" required />
+      <input 
+        name="password"
+        type="password"
+        autoComplete="password"
+        onChange={handleChange}
+        required
+      />
       <button type="submit">Se connecter</button>
     </form>
+    </>
   )
 }
 
