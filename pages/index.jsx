c616bf60eb58f20a58fa1f2168c1f6bf75c79fd6
Mathieu JOLY <m.joly@live.fr>
2021-03-22 22:55:05 +0100

    update default Home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,20 @@
 import React, { FunctionComponent } from 'react';
+import { useState, useEffect } from 'react';
+import Router from "next/router";
 import Head from 'next/head';
 import Layout from 'components/Layout';
 import styles from './Home.module.scss';
+import Cookie from 'js-cookie';
+
 
 const Home = () => {
+  const [role, setRole] = useState(Cookie.get('role'));
+
+  useEffect(() => {
+    if (role == 'employer') Router.push('/employer_home');
+    if (role == null) Router.push('/concept');
+  }, [role])
+
   return (
     <Layout>
     <div className={styles.Home}>
@@ -11,7 +22,6 @@ const Home = () => {
         <title>extra-resto - Home</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div className='main'>Hello from HomePage</div>
     </div>
     </Layout>
   );
