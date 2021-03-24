14b8e5caa348b87371c3e1fbd508c657812aee3f
stephen2790022 <toulouse.stephen1@gmail.com>
2021-03-24 16:53:26 +0100

    apply candidature

--- pages/candidate_home/index.jsx
+++ pages/candidate_home/index.jsx
@@ -4,7 +4,20 @@ import Head from 'next/head';
 import cookie from 'cookie';
 import styles from './CandidateHome.module.scss';
 
-const CandidateHome = ({jobListe}) => {
+const CandidateHome = ({jobListe, token}) => {
+
+  const handleCandidateApply = async (jobId) => {
+    
+    const response = await fetch(`${process.env.API_ROOT}candidatures`, {
+      method: 'POST',
+      headers: {
+        'Authorization': token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({jobs_id: jobId})
+    })
+  }
+
     return (
         <Layout>
             <Head>
@@ -21,6 +34,7 @@ const CandidateHome = ({jobListe}) => {
                         <p>Dresscode: {job.dresscode}</p>
                         <p>Nombre de place restante{job.free_stead}</p>
                         <p>Commence le {job.date}</p>
+                        <button onClick={() => handleCandidateApply(job.id)}>Apply</button>
                     </li>
                 ))
                 }
@@ -45,7 +59,8 @@ export const getServerSideProps = async ({req}) =>  {
   
     return {
       props: {
-        jobListe
+        jobListe,
+        token
       }
     }
   }
