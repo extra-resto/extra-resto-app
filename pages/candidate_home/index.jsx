6371fae746fb2ff6bbcc80fda866fc1de1d96ef0
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-03-28 00:15:03 +0100

    change card

--- pages/candidate_home/index.jsx
+++ pages/candidate_home/index.jsx
@@ -5,45 +5,49 @@ import cookie from 'cookie';
 import styles from './CandidateHome.module.scss';
 import Link from 'next/link';
 import config from 'config/config.json';
+import dayjs from 'dayjs';
+import 'dayjs/locale/fr';
+import Button from 'components/Button';
 
 const CandidateHome = ({ jobListe }) => {
+  dayjs.locale('fr')
   
     return (
-        <Layout>
-            <Head>
-                <title>Candidate home page</title>
-            </Head>
-            <div className={styles.bg}></div>
-        <div className={styles.CandidateHome}>
-          {console.log(jobListe)}
-            <h1>Liste des emplois</h1>
-            <ul>
-
-          <div className={styles.main}>
-                {jobListe && 
-                jobListe.map((job) => (
-                  
-                <li key={job.id} className={styles.card}>
-                  <div className={styles.card__header}>
-                    <h2>{job.name}</h2>
-                  </div>
-
-                  <div className={styles.card__body}>
-                    <p>Nombre de place restante: {job.free_stead}</p>
-                  </div>
-                  
-                  <div className={styles.card__footer}>
-                    <Link  href="/candidate_home/job/[id]" as={`/candidate_home/job/${job.id}`} passHref >
-                      <button>Voir le job</button>
-                    </Link>
-                  </div>
-                </li>
-          
-                ))
-                }
+      <Layout>
+        <Head>
+          <title>Candidate home page</title>
+        </Head>
+          <div className={styles.CandidateHome}>
+            <div className={styles.CandidateHome__title}>
+              <h1>Liste des emplois</h1>
             </div>
-            </ul>
-        </div> 
+            <div className={styles.CandidateHome__display}>
+              <ul className={styles.main}>
+                {jobListe && jobListe.map((job) => (
+                
+                  
+                  <li key={job.id} className={styles.card}>
+                    <div className={styles.card__header}>
+                      <h2>{job.name}</h2>
+                    </div>
+
+                    <div className={styles.card__body}>
+                      <p>{dayjs(job.date).format('DD MMMM YYYY')}</p>
+                      <p>Nombre de place restante : {job.free_stead}</p>
+                    </div>
+                    
+                    <div className={styles.card__footer}>
+                      <Link  href="/candidate_home/job/[id]" as={`/candidate_home/job/${job.id}`} passHref >
+                        <a><Button content="Voir le job" /></a>
+                      </Link>
+                    </div>
+                  </li>
+          
+                ))}
+              </ul>
+            </div>
+          </div> 
+
         </Layout>
 
     )
